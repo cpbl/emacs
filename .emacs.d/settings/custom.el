f6764a505c57bbac44bbd0ef816636405ff4c9b2
Chris Barrington-Leigh <cpbl@alum.mit.edu>
2014-08-07 14:09:00 -0400

    In org mode, start with long lines wrapped

--- .emacs.d/settings/custom.el
+++ .emacs.d/settings/custom.el
@@ -4,7 +4,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-return-follows-link t))
+ '(org-return-follows-link t)
+; Wrap long lines nicely, when displaying:
+ '(org-startup-truncated nil))
 
 ; color theme
 (add-to-list 'custom-theme-load-path (make-plugin-path "color-theme-solarized"))
